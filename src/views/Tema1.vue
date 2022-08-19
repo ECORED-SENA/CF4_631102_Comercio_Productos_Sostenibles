be958cbf6c85626a934cc541f24b2d767c0a7a38
Daniel Mutis <magdi.khalifah@misena.edu.co>
2022-08-19 16:28:34 -0500

    Videos

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -200,7 +200,7 @@
 
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)  
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/tTJ3Zgl1DRc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)         
 
     .row.justify-content-center(data-aos="zoom-in").mb-5
       .col-lg-10
